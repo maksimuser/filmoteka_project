f0e2eaf8d92fb13b09c3470643e85b6e475af8f3
Julia Fedyuk <fedyuk.julia@gmail.com>
2021-03-10 20:24:48 +0200

    добавляет  modal-team

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import apiService from './js/api-service';
 import updateTrendMarkup from './js/update-markup';
 import inputService from './js/input-service';
 import debounce from 'lodash.debounce';
+import lightbox from './js/modal-team';
 
 apiService.fetchTrendMovie().then(trendMovies => {
   updateTrendMarkup(trendMovies);
@@ -37,3 +38,5 @@ searchInput.addEventListener(
     }
   }, 1500),
 );
+
+lightbox();
